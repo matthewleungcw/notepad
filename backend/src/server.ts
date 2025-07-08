9d8fc1167cbf30e8e9496baabada2e8c7fc276a7
Matthew Leung <matthewleung26@gmail.com>
2025-07-08 22:15:54 +0100

    add for production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,18 +4,22 @@ import {connectDB} from "./config/db";
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter";
 import cors from "cors"
+import path from "path"
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT
+// const __dirname = path.resolve()
 
 // Middleware
-app.use(cors(
-    {
-        origin:"http://localhost:5173"
-    }
-))
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors(
+        {
+            origin:"http://localhost:5173"
+        }
+    ))
+}
 app.use(express.json()) // middleware parse JSON bodies (req.body)
 app.use(rateLimiter)
 
@@ -26,6 +30,14 @@ app.use(rateLimiter)
 // })
 
 app.use("/api/notes", routes);
+
+if(process.env.NODE_ENV === "production" ) {
+    app.use(express.static(path.join(__dirname,"../../frontend/dist")))
+
+    app.get("*", (req: Request, res: Response) => {
+        res.sendFile(path.join(__dirname,"../../frontend", "dist", "index.html"))
+    })
+}
 
 // Connect to port after connecting DB
 connectDB().then(() => {
